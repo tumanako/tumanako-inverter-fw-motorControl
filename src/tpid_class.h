b3897a062eebcaa5653c817f4f4078398f655f69
Donovan Johnson <donovan.johnson@gmail.com>
2010-05-26 10:57:12 +0000

    Updated to be inline with google C++ rules, might be more to do. Moved inline functions into header file.
    git-svn-id: tumanako.svn.sourceforge.net::svn/tumanako/trunk@252 c3429617-7ab9-49b5-8233-5f90b2ce515f

--- src/tpid_class.h
+++ src/tpid_class.h
@@ -35,80 +35,83 @@
 #define	_TPID_CLASS_H
 
 class _pid{
-private:
-	float pg;           // proportional gain
-	float ig;           // integral gain
-	float dg;           // derivitive gain
-	float accel;        // Acceleration limit (rate of change)
-	float sp;           // User setpoint
-	float min_val;      // minimum setpoint
-	float max_val;      // maximum setpoint
-	float err;          // current error
-	float last_err;     // previous error
-	float i;            // integral
-	float last_pp;      // last process point
-	float pp;           // current process point
-	float next_sp;      // next setpoint
-	float this_sp;      // current setpoint
-	float last_sp;      // last setpoint
-	float result;       // current PId output
-	float last_result;  // previous PID output
-public:
+ private:
+  float pg_;					// proportional gain
+  float ig_;					// integral gain
+  float dg_;					// derivitive gain
+  float acceleration_limit_;	// Acceleration limit (rate of change)
+  float setpoint_;				// User setpoint
+  float minimum_pid_;			// minimum setpoint
+  float maximum_pid_;			// maximum setpoint
+  float current_err_;			// current error
+  float last_err_;				// previous error
+  float i_;						// integral
+  float last_process_point_;    // last process point
+  float process_point_;         // current process point
+  float next_setpoint_;			// next setpoint
+  float this_setpoint_;			// current setpoint
+  float last_setpoint_;			// last setpoint
+  float pid_result_;			// current PId output
+  float last_pid_result_;		// previous PID output
+ public:
+  // Contstructor & Destructor
+  _pid();
+  ~_pid(){};	
 	
-    // Function constructors
-    void init_pid(int, int, float, float, float);
-    void tune_pid(float, float, float);
-    void pid_setintegral(float);
-    float calc_pid();
+  // Inline getters and setters
+  inline const float get_setpoint(){ return setpoint_; }
+  inline void set_setpoint(float sp){ setpoint_ = setpoint_; }
+  inline const float get_this_setpoint(){ return this_setpoint_; }
+  inline void set_this_setpoint(float tsp){ this_setpoint_ = tsp; }
+  inline const float get_next_setpoint(){ return next_setpoint_; }
+  inline void set_next_setpoint(float nsp){ next_setpoint_ = nsp; }
+  inline const float get_last_setpoint(){ return last_setpoint_; }
+  inline void set_last_setpoint(float lsp){ last_setpoint_ = lsp; }
+  inline const float get_process_point(){ return process_point_; }
+  inline void set_process_point(float pp){ process_point_ = pp; }
+  inline const float get_last_process_point(){ return last_process_point_; }
+  inline void set_last_process_point(float lpp){ last_process_point_ = lpp; }
+  inline const float get_acceleration_limit(){ return acceleration_limit_; }
+  inline void set_acceleration_limit(float acc){ acceleration_limit_ = acc; }
+  inline const float get_minimum_pid(){ return minimum_pid_; }
+  inline void set_minimum_pid(float min){ minimum_pid_ = min; }
+  inline const float get_maximum_pid(){ return maximum_pid_; }
+  inline void set_maximum_pid(float max){ maximum_pid_ = max; }
+  inline const float get_pid_result(){ return pid_result_; }
+  inline void set_pid_result(float sr){ pid_result_ = sr; }
+  inline const float get_last_pid_result(){ return last_pid_result_; }
+  inline void set_last_pid_result(float lr){ last_pid_result_ = lr; }
+  inline const float get_current_error(){ return current_err_; }
 	
-    // Getter setter for user setpoint
-    void setsp(float);
-    float getsp();
+	//---------------------------------------------------------------------------
+  // TUNE_PID
+  // inputs: proportional gain, integral gain, derivitive gain
+  // outputs: none
+  // process: Updates variables in _pid class with new values, used for tuning
+  //---------------------------------------------------------------------------
+	inline void tune_pid(float p_g, float i_g, float d_g)
+	{
+		pg_ = p_g;
+		ig_ = i_g;
+		dg_ = d_g;
+	}
 	
-    // Getter setter for current target setpoint
-    void setthissp(float);
-    float getthissp();
+	//------------------------------------------------------------------------
+  // PID_SET_INTEGRAL
+  // inputs:  New integral value
+  // outputs: none
+  // process: Sets integral to input value, resets last error to zero
+  // usage: Use to reset pid control when starting up
+  //------------------------------------------------------------------------//
+	inline void _pid::pid_set_integral(float ni)
+	{
+		i_ = ni;
+		last_err_ = 0.0;
+	}
 	
-    // Getter setter for next target setpoint
-    void setnextsp(float);
-    float getnextsp();
-	
-    // Getter setter for last target setpoint
-    void setlastsp(float);
-    float getlastsp();
-	
-    // Getter setter for current process point
-    void setpp(float);
-    float getpp();
-	
-    // Getter setter for last process point
-    void setlastpp(float);
-    float getlastpp();
-	
-    // Getter setter for acceleration limiting
-    void setaccel(float);
-    float getaccel();
-	
-    // Getter setter for pid output minimum
-    void setmin(float);
-    float getmin();
-	
-    // Getter setter for pid output maximum
-    void setmax(float);
-    float getmax();
-	
-    // Getter setter for current pid output result
-    void setresult(float);
-    float getresult();
-	
-    // Getter setter for last pid output result
-    void setlastresult(float);
-    float getlastresult();
-	
-    // Getter for current error
-    float geterr();
+	// Methods constructors
+	void init_pid(int, int, float, float, float);
+	float calc_pid();
 };
-
-
 #endif	/* _TPID_CLASS_H */
 
