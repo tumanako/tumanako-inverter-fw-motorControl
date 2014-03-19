8c0c98de4f384579bcb0c8a3dc98e8443a79d3ca
jsphuebner <dev@johanneshuebner.com>
2014-03-19 23:19:40 +0100

    and another one

--- src/sine/digio_prj.h
+++ src/sine/digio_prj.h
@@ -1,43 +1,44 @@
-#ifndef DIGIO_PRJ_H_INCLUDED
-#define DIGIO_PRJ_H_INCLUDED
+#ifndef PinMode_PRJ_H_INCLUDED
+#define PinMode_PRJ_H_INCLUDED
 
 #include "hwdefs.h"
 
 #if (HWCONFIG == HWCONFIG_TUMANAKO_KIWIAC)
 
 #define DIG_IO_LIST \
-    DIG_IO_ENTRY(dcsw_out,    GPIOC, GPIO11, MODE_OUTPUT)      \
-    DIG_IO_ENTRY(err_out,     GPIOC, GPIO6,  MODE_OUTPUT)      \
-    DIG_IO_ENTRY(ovtmp_out,   GPIOC, GPIO11, MODE_OUTPUT)      \
-    DIG_IO_ENTRY(prec_out,    GPIOD, GPIO6,  MODE_OUTPUT)      \
-    DIG_IO_ENTRY(led_out,     GPIOC, GPIO7,  MODE_OUTPUT)      \
-    DIG_IO_ENTRY(on_in,       GPIOB, GPIO5,  MODE_INPUT_PD)    \
-    DIG_IO_ENTRY(start_in,    GPIOD, GPIO6,  MODE_INPUT_PD)    \
-    DIG_IO_ENTRY(brake_in,    GPIOG, GPIO2,  MODE_INPUT_PD)    \
-    DIG_IO_ENTRY(mprot_in,    GPIOG, GPIO3,  MODE_INPUT_PD)    \
-    DIG_IO_ENTRY(fwd_in,      GPIOG, GPIO10, MODE_INPUT_PD)    \
-    DIG_IO_ENTRY(rev_in,      GPIOG, GPIO11, MODE_INPUT_PD)    \
-    DIG_IO_ENTRY(emcystop_in, GPIOC, GPIO7,  MODE_INPUT_PD)    \
-    DIG_IO_ENTRY(bms_in,      GPIOA, GPIO11, MODE_INPUT_PD)    \
+    DIG_IO_ENTRY(dcsw_out,    GPIOC, GPIO11, PinMode::OUTPUT)      \
+    DIG_IO_ENTRY(err_out,     GPIOC, GPIO6,  PinMode_OUTPUT)      \
+    DIG_IO_ENTRY(ovtmp_out,   GPIOC, GPIO11, PinMode_OUTPUT)      \
+    DIG_IO_ENTRY(prec_out,    GPIOD, GPIO6,  PinMode_OUTPUT)      \
+    DIG_IO_ENTRY(led_out,     GPIOC, GPIO7,  PinMode_OUTPUT)      \
+    DIG_IO_ENTRY(on_in,       GPIOB, GPIO5,  PinMode_INPUT_PD)    \
+    DIG_IO_ENTRY(start_in,    GPIOD, GPIO6,  PinMode_INPUT_PD)    \
+    DIG_IO_ENTRY(brake_in,    GPIOG, GPIO2,  PinMode_INPUT_PD)    \
+    DIG_IO_ENTRY(mprot_in,    GPIOG, GPIO3,  PinMode_INPUT_PD)    \
+    DIG_IO_ENTRY(fwd_in,      GPIOG, GPIO10, PinMode_INPUT_PD)    \
+    DIG_IO_ENTRY(rev_in,      GPIOG, GPIO11, PinMode_INPUT_PD)    \
+    DIG_IO_ENTRY(emcystop_in, GPIOC, GPIO7,  PinMode_INPUT_PD)    \
+    DIG_IO_ENTRY(bms_in,      GPIOA, GPIO11, PinMode_INPUT_PD)    \
 
 
 #elif (HWCONFIG == HWCONFIG_OLIMEX)
 
 #define DIG_IO_LIST \
-    DIG_IO_ENTRY(dcsw_out,    GPIOC, GPIO13, MODE_OUTPUT)      \
-    DIG_IO_ENTRY(err_out,     GPIOC, GPIO10, MODE_OUTPUT)      \
-    DIG_IO_ENTRY(ovtmp_out,   GPIOC, GPIO11, MODE_OUTPUT)      \
-    DIG_IO_ENTRY(prec_out,    GPIOB, GPIO1,  MODE_OUTPUT)      \
-    DIG_IO_ENTRY(led_out,     GPIOC, GPIO12, MODE_OUTPUT)      \
-    DIG_IO_ENTRY(on_in,       GPIOB, GPIO5,  MODE_INPUT_PU)    \
-    DIG_IO_ENTRY(start_in,    GPIOB, GPIO6,  MODE_INPUT_PU)    \
-    DIG_IO_ENTRY(brake_in,    GPIOA, GPIO2,  MODE_INPUT_PU)    \
-    DIG_IO_ENTRY(mprot_in,    GPIOA, GPIO3,  MODE_INPUT_PU)    \
-    DIG_IO_ENTRY(fwd_in,      GPIOA, GPIO4,  MODE_INPUT_PU)    \
-    DIG_IO_ENTRY(rev_in,      GPIOC, GPIO6,  MODE_INPUT_PU)    \
-    DIG_IO_ENTRY(emcystop_in, GPIOC, GPIO7,  MODE_INPUT_PU)    \
-    DIG_IO_ENTRY(bms_in,      GPIOC, GPIO8,  MODE_INPUT_PU)    \
+    DIG_IO_ENTRY(dcsw_out,    GPIOC, GPIO13, PinMode::OUTPUT)      \
+    DIG_IO_ENTRY(err_out,     GPIOC, GPIO10, PinMode::OUTPUT)      \
+    DIG_IO_ENTRY(vtg_out,     GPIOC, GPIO11, PinMode::OUTPUT)      \
+    DIG_IO_ENTRY(prec_out,    GPIOB, GPIO1,  PinMode::OUTPUT)      \
+    DIG_IO_ENTRY(led_out,     GPIOC, GPIO12, PinMode::OUTPUT)      \
+    DIG_IO_ENTRY(on_in,       GPIOB, GPIO5,  PinMode::INPUT_PD)    \
+    DIG_IO_ENTRY(start_in,    GPIOB, GPIO6,  PinMode::INPUT_PD)    \
+    DIG_IO_ENTRY(brake_in,    GPIOA, GPIO2,  PinMode::INPUT_PD)    \
+    DIG_IO_ENTRY(mprot_in,    GPIOA, GPIO3,  PinMode::INPUT_PD)    \
+    DIG_IO_ENTRY(fwd_in,      GPIOA, GPIO4,  PinMode::INPUT_PD)    \
+    DIG_IO_ENTRY(rev_in,      GPIOC, GPIO6,  PinMode::INPUT_PD)    \
+    DIG_IO_ENTRY(emcystop_in, GPIOC, GPIO7,  PinMode::INPUT_PD)    \
+    DIG_IO_ENTRY(bk_in,       GPIOB, GPIO12, PinMode::INPUT_FLT)   \
+    DIG_IO_ENTRY(bms_in,      GPIOC, GPIO8,  PinMode::INPUT_PD)    \
 
 #endif
 
-#endif // DIGIO_PRJ_H_INCLUDED
+#endif // PinMode_PRJ_H_INCLUDED
