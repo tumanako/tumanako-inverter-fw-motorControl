0cba82522e2c5ba449740b94da310ebba2475f16
johannes <dev@johanneshuebner.com>
2016-11-02 12:30:17 +0100

    - Added basic CAN functionality

---
+++ src/sine/stm32_can.h
@@ -0,0 +1,16 @@
+#ifndef STM32_CAN_H_INCLUDED
+#define STM32_CAN_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void can_setup(void);
+void can_send(uint32_t canId, uint8_t* data, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
